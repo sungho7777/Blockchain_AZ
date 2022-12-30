0668a108cc70f3c35d526720224040d619fa48fc
mzncvmc <mzncvmc@gmail.com>
2022-12-30 22:18:28 +0900

    # Part 2 - Mining our Blockchain

--- Module 1 - Create a Blockchan/blockchain.py
+++ Module 1 - Create a Blockchan/blockchain.py
@@ -17,17 +17,16 @@ from flask import Flask, jsonify
 # -------------------------------------------------------
 
 class Blockchain:
+    
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
     
     def create_block(self, proof, previous_hash):
-        block = {
-            'index': len(self.chain) + 1,
-            'timestamp': str(datetime.datetime.now()),
-            'proof': proof,
-            'previous_hash': previous_hash,
-            }
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
         self.chain.append(block)
         return block
     
@@ -39,14 +38,14 @@ class Blockchain:
         check_proof = False
         while check_proof is False:
             hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
-            if hash_operation[:4] == '00000':
-                check_proof == True
+            if hash_operation[:4] == '0000':
+                check_proof = True
             else:
                 new_proof += 1
         return new_proof
                 
     def hash(self, block):
-        encoded_block = json.dump(block, sort_keys = True).encode()
+        encoded_block = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
     
     def is_chain_valid(self, chain):
@@ -65,14 +64,44 @@ class Blockchain:
             block_index += 1
         return True
 
-
-
 # -------------------------------------------------------
 #
 # Part 2 - Mining our Blockchain
-#
+# : Flask
+#   https://flask.palletsprojects.com/en/2.2.x/quickstart/
 # -------------------------------------------------------
 
+# Creating a Web App
+app = Flask(__name__)
+#app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+# Creating a Blockchain
+blockchain =  Blockchain()
+
+# Mining a new block
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message':'Congratulation, you just mined a block',
+               'index': block['index'],
+               'timestamp': block['timestamp'],
+               'proof': block['proof'],
+               'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+# Getting the full Blockchain
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+# Running the app
+app.run(host = '0.0.0.0', port = 5000)
 
 
 
@@ -100,3 +129,34 @@ class Blockchain:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# end
